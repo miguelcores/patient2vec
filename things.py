a8b9f1b01d7639d0932de7e24c2393c454ae1212
MIGUEL CORES <micores@cisco.com>
2020-09-06 13:31:07 +0100

    edited things

--- things.py
+++ things.py
@@ -3,7 +3,6 @@ from Common import utils
 # translator = utils.load_object('./_data/hp-obo-translator.pkl')
 # # print(translator['id2name'])
 #
-# objeto = utils.load_object('./_data/walks/walks.pkl')
 #
 # for i in objeto:
 #     if i[0] == '8317':
